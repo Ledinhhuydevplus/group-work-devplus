008f0ef5e29315e4cd0206cb4cd325f7766616e8
nhanVoThanhdp05 <nhan.vo_dp05.devplus.edu.vn>
2023-04-03 13:11:51 +0700

    feat: dpm-add-webpage-icon

--- src/components/FAQ/FAQ.js
+++ src/components/FAQ/FAQ.js
@@ -22,7 +22,7 @@ export default function FAQ() {
     <Row className="faq-section">
       <Col xl={24} lg={24} md={24} sm={24} className="custom-container">
         <Row>
-          <Col xl={12} lg={12} md={24} sm={24} className="left-section">
+          <Col xl={12} lg={24} md={24} sm={24} className="left-section">
             <Row className="left-section-title-wrapper">
               <Col lg={24} md={24} sm={24} xs={24}>
                 <Typography.Text className="left-section-title">
@@ -51,7 +51,7 @@ export default function FAQ() {
                       header={item.name}
                       key={index}
                       className={
-                        index == activeKey
+                        index.toString() === activeKey.toString()
                           ? "custom-active-collapse-panel"
                           : "custom-collapse-panel"
                       }
@@ -67,7 +67,7 @@ export default function FAQ() {
           </Col>
           <Col
             xl={12}
-            lg={12}
+            lg={24}
             md={24}
             className="right-section"
             style={{
