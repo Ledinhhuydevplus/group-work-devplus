c577a0237eacb663e48f0744e0d019fcc050a70a
anhhuy0168 <anhhuy123zxc123@gmail.com>
2023-04-03 13:40:13 +0700

    feat/dpm:Done-road-map

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import 'antd/dist/antd.css';
-import HomePage from './pages/HomePage';
-
+import "antd/dist/antd.css";
+import HomePage from "./pages/HomePage";
+import OnePlusCampus from "./components/RoadMap/OnePlus_RoadMap/OnePlus_RoadMap";
 function App() {
   return (
-    <>
+    <div className="">
       <HomePage />
-    </>
+      <OnePlusCampus />
+    </div>
   );
 }
 
