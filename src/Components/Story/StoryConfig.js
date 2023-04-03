d149f5ce9548abf5d8946c6b2931d239211992e5
Ledinhhuydevplus <126637247+Ledinhhuydevplus@users.noreply.github.com>
2023-04-03 14:33:01 +0700

    feat: dpm-alumni-saying (#3)

---
+++ src/Components/Story/StoryConfig.js
@@ -0,0 +1,17 @@
+export const settings = {
+    dots: true,
+    infinite: true,
+    speed: 2000,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    swipeToSlide: false,
+    cssEase: "linear",
+    responsive: [
+      {
+        breakpoint: 678,
+        settings: {
+          slidesToShow: 0,
+        },
+      },
+    ],
+  };
